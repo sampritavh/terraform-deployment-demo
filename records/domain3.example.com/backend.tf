2c38c580e6035820ff80727ff72421d9264a1332
Samprita Hegde <hegde.samprita@gmail.com>
2023-12-19 19:09:30 +0530

    update backend configuration

--- records/domain3.example.com/backend.tf
+++ records/domain3.example.com/backend.tf
@@ -1,7 +1,9 @@
-backend "s3" {
-  region         = "us-east-1"
-  bucket         = "gitops-demo-195368226277"
-  key            = "${locals.domain_name}/terraform.tfstate"
-  dynamodb_table = "gitops-demo-195368226277-state-lock"
-  encrypt        = true
+terraform {
+  backend "s3" {
+    region         = "us-east-1"
+    bucket         = "gitops-demo-195368226277"
+    key            = "domain3.example.com/terraform.tfstate"
+    dynamodb_table = "gitops-demo-195368226277-state-lock"
+    encrypt        = true
+  }
 }
